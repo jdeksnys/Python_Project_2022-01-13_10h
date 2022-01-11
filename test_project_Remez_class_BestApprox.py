e67298a4a2bf0a526f8c9c29e27e4a064de436a1
Jonas Deksnys <jonasdeksnys@gmail.com>
2022-01-11 15:31:42 +0100

    refresh

--- test_project_Remez_class_BestApprox.py
+++ test_project_Remez_class_BestApprox.py
@@ -1,2 +1,18 @@
 import os
 os.system('Clear')
+import unittest
+
+import numpy as np
+from project_Remez_class_BestApprox import BestApprox
+
+def banana():
+    return x**2
+
+class TestBestApprox():
+    def test___init__(self,function):
+        self.assertRaises(Exception,__init__,x**2)
+        #self.f='a'
+    def test_remez(self):
+        self.assertRaises(Exception,__init__)
+        #self.f='a'
+    
