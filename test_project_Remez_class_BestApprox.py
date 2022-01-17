2869bea42101979a8fd9c71b3f4e2cd7f7becfe6
Saninaa <96122693+Saninaa@users.noreply.github.com>
2022-01-17 01:25:14 +0100

    Update test_project_Remez_class_BestApprox.py

--- test_project_Remez_class_BestApprox.py
+++ test_project_Remez_class_BestApprox.py
@@ -7,23 +7,23 @@ import numpy as np
 class TestBestApprox(unittest.TestCase):
 
     def test_remez(self):
-        reference_points='abc'
-        def f(x):
-            return np.sin(x)
-        a=project_Remez_class_BestApprox.BestApprox(f)
-        self.assertRaises(TypeError,a.remez,reference_points)
+        func1=[np.sin,np.cos,np.tan]
+        for n in func1:
+            reference_points='abc'
+            a=project_Remez_class_BestApprox.BestApprox(f)
+            self.assertRaises(TypeError,a.remez,reference_points)
+        
 
     def test_signs(self):
-        def f(x):
-            return np.sin(x)
-        reference_points=list(np.linspace(0,2*np.pi,6))
-        a=project_Remez_class_BestApprox.BestApprox(f)
-        a.remez(reference_points)
-
-        for i in range(0,len(a.h_vals)-1):
-            if (a.h_vals[i]/a.h_vals[i+1])<0:
+        func=[np.sin,np.cos,np.tan]
+        for n in func:
+            reference_points=list(np.linspace(0,2*np.pi,6))
+            a=project_Remez_class_BestApprox.BestApprox(f)
+            a.remez(reference_points)
+            for i in range(0,len(a.h_vals)-1):
+              if (a.h_vals[i]/a.h_vals[i+1])<0:
                 pass
-            else:
+              else:
                 raise Exception('h-values not alternating in sign!')
                  
                  
